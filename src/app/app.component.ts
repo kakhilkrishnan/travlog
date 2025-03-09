4390a522a24e176a6402d8fe3a70cb230e5b85c2
akhilkrishnan k <k.akhilkrishnan@gmail.com>
2025-03-09 17:32:07 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'travlog';
+
+  images = [
+    '../assets/images/image1.jpg',
+    '../assets/images/image2.jpg',
+    '../assets/images/image3.jpg',
+  ];
+
+  leaders = [
+    { name: 'Rajeev Pradeep', photo: '../assets/images/human1.jpg' },
+    { name: 'Abey Issac', photo: '../assets/images/human2.jpg' },
+    { name: 'Sanyo', photo: '../assets/images/human3.jpg' },
+  ];
+
 }
